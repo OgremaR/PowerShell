afaa58b35d932f352a65c0b50f439a58f64b5263
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-03-03 12:16:48 +0300

    Add files via upload

---
+++ Forming_the_path.ps1
@@ -0,0 +1,12 @@
+﻿# Forming the path to the file
+[string]$Path1 = 'C:\Users'
+[string]$Path2 = '\AppData\Roaming\Yandex\YandexBrowser\User Data\Default\Bookmarks'
+$user = whoami
+$user = $user.Substring(3)
+$DestinationFile = "$($Path1 + $user + $Path2)"
+
+# If there is a file, then ...
+If ((Test-Path $DestinationFile) -eq $True) {
+
+$DestinationFile #outputs the path itself
+}

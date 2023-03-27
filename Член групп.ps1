0f50ce8c88f58b4e205f63d97df1d92372fc10aa
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-03-27 13:45:06 +0300

    Add files via upload

---
+++ Член групп.ps1
@@ -0,0 +1,11 @@
+﻿$user_name = "sidorov_in" 
+# Read-Host -Prompt 'Write user name - '
+$text = Get-ADPrincipalGroupMembership $user_name | select name
+Write-Output $text > .\groups.txt
+$groups = (Get-Content .\groups.txt).replace('  ' , '')
+#Get-ADGroup -LDAPFilter “(name=*dfs*)” | Format-Table
+foreach ($group in $groups) {
+    $group = $group -replace ".{1}$"
+    $data = Get-ADPrincipalGroupMembership $group | select name 2>$null
+    Write-Output $data >> .\groups.txt
+}

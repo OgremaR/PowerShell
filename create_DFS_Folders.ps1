20d5d26b20b60453a875d800d5d778336364dd45
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-04-14 13:21:00 +0300

    Delete create_DFS_Folders.ps1

--- create_DFS_Folders.ps1
+++
@@ -1,8 +0,0 @@
-﻿$pathdfs = "\\xxx.ru\DFS\xxx\"
-$pathtarget = "\\xxx\groups$\shares\xxx"
-$parts = Get-Content .\folders.txt
-foreach ($part in $parts) {
-    $pathd = $pathdfs+$part
-    $patht = $pathtarget+$part
-    New-DfsnFolder -Path $pathd -TargetPath $patht -EnableTargetFailback $false
-}

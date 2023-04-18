113db4fe06fcec02ca011a2b97bbf3bc4b9baa03
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-04-18 16:50:43 +0300

    Add files via upload

---
+++ create_AD_Groupa.ps1
@@ -0,0 +1,9 @@
+﻿$parts = Get-Content .\folders.txt
+$gp = "dfs_ap_ec_"
+$OU = "OU=Export Clients,OU=xxx-Pak,OU=DFS,OU=xxxxxxx,DC=XXXXdomen,DC=xxxxx-pak,DC=ru"
+foreach ($part in $parts) {
+    $gpRead = $gp+$part+"_read"
+    $gpWrite = $gp+$part+"_write"
+    New-ADGroup -Name $gpRead -SamAccountName $gpRead -GroupCategory Security -GroupScope DomainLocal -DisplayName $gpRead -Path $OU -Description ""
+    New-ADGroup -Name $gpWrite -SamAccountName $gpWrite -GroupCategory Security -GroupScope DomainLocal -DisplayName $gpWrite -Path $OU -Description ""
+}

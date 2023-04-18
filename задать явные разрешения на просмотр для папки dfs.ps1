ab0c92ed67a8ae52666cf785e2d0d8739273fac9
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-04-18 16:51:32 +0300

    Add files via upload

---
+++ задать явные разрешения на просмотр для папки dfs.ps1
@@ -0,0 +1,9 @@
+﻿$parts = Get-Content .\folders.txt
+$gp = "dfs_xx_xx_"
+foreach ($part in $parts) {
+    $pathdfs = "\\xxxx.ru\DFS\xxxx-Pak\xxxxx\"+$part
+    $gpRead = $gp+$part+"_read"+":RX"
+    $gpWrite = $gp+$part+"_write"+":RX"
+    dfsutil property sd grant $pathdfs "$gpRead" Protect Replace
+    dfsutil property sd grant $pathdfs "$gpWrite" Protect Replace
+}

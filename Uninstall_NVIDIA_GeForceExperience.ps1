e12844dad8e3528d850404cbec417673b570af65
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-05-22 12:38:48 +0300

    Add files via upload

---
+++ Uninstall_NVIDIA_GeForceExperience.ps1
@@ -0,0 +1,18 @@
+﻿#Start-Process -FilePath C:\WINDOWS\SysWOW64\RunDll32.EXE -ArgumentList '"C:\Program Files\NVIDIA Corporation\Installer2\InstallerCore\NVI2.DLL",UninstallPackage Display.GFExperience -silent'
+Invoke-Command –ScriptBlock { 
+    $date = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+    $apps = get-childitem "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty -ErrorAction SilentlyContinue  
+
+    foreach ($app in $apps) { 
+        if ($app.DisplayName -like "*GeForce Experience*") { 
+            if ($app.UninstallString) { $us = $app.UninstallString } else { $us = $app.UninstallString_Hidden } 
+            if ($us -ne '') { 
+                $app.DisplayName + ' ' + $us 
+                $unused,$filepath,$arglist = $us -split '"', 3 
+                $arglist = $arglist.Trim() + ' -silent' 
+                Start-Process -FilePath $filepath -ArgumentList $arglist 2>&1|
+		        Out-File -Append \\winsystem\otherdata$\storages\RemoteScriptLogs\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+            } 
+        }
+    }
+}
---
+++ universal_uninstall_of_programs.ps1
@@ -0,0 +1,37 @@
+﻿$date = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$regNames = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\' -Include "*{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}_*" -name
+$regPath = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"
+
+Get-Service -DisplayName "NVIDIA*"  | Stop-Service
+Get-Process "NVIDIA Web Helper" | Stop-Process -Force
+
+#Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*}_NV*"
+
+foreach ($name in $regNames) {
+    $regPath = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + $name
+    if (reg query "$regPath" /reg:64) {
+    reg delete "$regPath" /f /reg:64
+    }
+    else {
+    reg delete "$regPath" /f /reg:32 2>&1|
+    Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+    }
+}
+reg delete "HKLM\SOFTWARE\NVIDIA Corporation\Global\GFExperience" /f /reg:64 2>&1|
+Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+reg delete "HKLM\SOFTWARE\WOW6432Node\NVIDIA Corporation\Global\GFExperience" /f /reg:64 2>&1|
+Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+
+reg delete "HKLM\SOFTWARE\NVIDIA Corporation\Global\GFExperience" /f /reg:32 2>&1|
+Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+reg delete "HKLM\SOFTWARE\WOW6432Node\NVIDIA Corporation\Global\GFExperience" /f /reg:32 2>&1|
+Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+
+if ((Test-Path -Path "C:\Program Files\NVIDIA Corporation\") -eq $True){
+    Remove-Item -LiteralPath "C:\Program Files\NVIDIA Corporation\" -Force -Recurse 2>&1|
+    Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+}
+if ((Test-Path -Path "C:\Program Files (x86)\NVIDIA Corporation\") -eq $True){
+    Remove-Item -LiteralPath "C:\Program Files (x86)\NVIDIA Corporation\" -Force -Recurse 2>&1|
+    Out-File -Append \\XXX\Uninstall_GeForceExperience_Log\$env:computername-"("$Date")".txt
+}

4947616c905dbf568321b71c7475a34f73066192
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-06-02 12:47:00 +0300

    Add files via upload

---
+++ Перебор реестра на компютере с переменной.ps1
@@ -0,0 +1,11 @@
+﻿$Date = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$computers = Get-Content -Path "\\pc-01\exchange\pc\pc.txt"
+$computers = $computers.Trim()
+
+foreach ($computer in $computers) {
+    $temp = Invoke-Command -ComputerName $computer -ScriptBlock {get-childitem "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" -name}
+    if ($temp -like "*GFExperience*") {
+        $computer >> \\pc-01\exchange\pc\\$env:computername-"("$Date")".md
+        $temp >> \\pc-01\exchange\pc\\$env:computername-"("$Date")".md
+        }
+}

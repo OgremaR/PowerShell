e3bbc4afabdf258708c2026e4a339f2f4e19efb9
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-06-13 14:44:13 +0300

    Add files via upload

---
+++ copy_and_report.ps1
@@ -0,0 +1,7 @@
+﻿$path_a = "C:\exchange\Directum\DirectumLauncher"
+$path_b = "C:\exchange\Directum\DirectumLauncher2"
+$start = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$start | Out-File -Append .\"("copying_info_$start")".txt
+Copy-Item -Path $path_a -Destination $path_b -Recurse
+$end = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$end | Out-File -Append .\"("copying_info_$start")".txt

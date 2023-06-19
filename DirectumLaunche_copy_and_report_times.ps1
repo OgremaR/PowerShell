b53a59183b4332611fdc621c26bca5949defcc2a
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-06-19 12:45:00 +0300

    Add files via upload

---
+++ DirectumLaunche_copy_and_report_times.ps1
@@ -0,0 +1,7 @@
+﻿$path_a = "\\any\directory\DirectumLauncher"
+$path_b = "\\anyPC\directory\"
+$start = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$start | Out-File -Append "\\anyPC\directory\(start_copying_DirectumLauncher_$start).txt"
+Copy-Item -Path $path_a -Destination $path_b -Recurse
+$end = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+$end | Out-File -Append "\\anyPC\directory\(end_copying_DirectumLauncher_$end).txt"

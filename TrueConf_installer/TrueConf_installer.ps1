52a1c9d74902363f5fbc7975f679de10f74f3bd2
OgremaR <106409011+OgremaR@users.noreply.github.com>
2023-11-02 10:53:08 +0300

    Add files via upload

---
+++ TrueConf_installer/TrueConf_installer.ps1
@@ -0,0 +1,14 @@
+﻿$PCs = Get-Content \\xxxxxx\TrueConf\computers.txt
+$date = Get-Date -Format dd-MM-yyyy-HH-mm-ss
+
+foreach ($PC in $PCs) {
+    if (Test-Connection $PC 2>$null){
+    Copy-Item -Path \\xxxxxx\TrueConf\Runer_TrueConf.bat -Destination \\$PC\c$\TrueConf\ -Recurse
+	Copy-Item -Path \\xxxxxx\TrueConf\trueconf_client_x64.exe -Destination \\$PC\c$\TrueConf\ -Recurse
+    Invoke-Command -ComputerName $PC -ScriptBlock {
+    & "C:\TrueConf\Runer_TrueConf.bat"
+    Remove-Item -Recurse C:\TrueConf\
+    }}
+    else {"$PC is not available!!! _______" |  Out-File -Append \\xxxxxx\TrueConf\$date.txt}
+    "$PC" |  Out-File -Append \\xxxxxx\TrueConf\$date.txt
+}

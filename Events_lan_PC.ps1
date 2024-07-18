455540f8bfb12e84373da23991e3ef4ad1a711bd
OgremaR <106409011+OgremaR@users.noreply.github.com>
2024-07-18 12:45:46 +0300

    Add files via upload

---
+++ Events_lan_PC.ps1
@@ -0,0 +1,2 @@
+﻿Get-EventLog -ComputerName PC-DC4 System #| Select-Object -Property EventID, TimeGenerated, @{e={$_.ReplacementStrings[0]}} | 
+    Out-File -FilePath C:\Users\_sidorov\Desktop\EventLogAD.txt

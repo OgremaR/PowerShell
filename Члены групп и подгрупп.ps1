419a76bc9b3b357264b73ad4f9c6cb2514546335
OgremaR <106409011+OgremaR@users.noreply.github.com>
2024-07-18 12:33:54 +0300

    Add files via upload

---
+++ Члены групп и подгрупп.ps1
@@ -0,0 +1,11 @@
+﻿$name = "uzb"
+$group_name = "dfs_ap_ec_" + "$name" + "_write" # Read-Host -Prompt 'Write user name - '
+$groups = Get-ADGroupMember $group_name | select name
+Write-Output $groups >> .\$group_name.txt
+foreach ($group in $groups) {
+    $group = $group -replace '@{name=', ''
+    $group = $group -replace '}', ''
+    $data = Get-ADGroupMember "$group" | select name
+    Write-Output $group >> .\$group_name.txt
+    Write-Output $data >> .\$group_name.txt
+}
